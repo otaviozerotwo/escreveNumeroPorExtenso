5a8954ac3d90b54e3bf72d4811c555c7ff6af7ab
otaviozerotwo <deadplatypus@protonmail.com>
2022-02-14 23:16:44 -0300

    segundo teste usando um dicionário com dez números

--- escreveNumeroPorExtenso.py
+++ escreveNumeroPorExtenso.py
@@ -1,27 +1,62 @@
-num = int(input('Digite um número:' ))
+"""
+Escreva um número por extenso.
 
-if(num <= 10 and num >= 0):
-    if(num == 10):
-        num_extenso = 'dez'
-    elif(num == 9):
-        num_extenso = 'nove'
-    elif(num == 8):
-        num_extenso = 'oito'
-    elif(num == 7):
-        num_extenso = 'sete'
-    elif(num == 6):
-        num_extenso = 'seis'
-    elif(num == 5):
-        num_extenso = 'cinco'
-    elif(num == 4):
-        num_extenso = 'quatro'
-    elif(num == 3):
-        num_extenso = 'três'
-    elif(num == 2):
-        num_extenso = 'dois'
-    elif(num == 1):
-        num_extenso = 'um'
+"""
+
+""" Implementação usando if else."""
+
+# num = int(input('Digite um número: '))
+
+# if(num <= 10 and num >= 0):
+#     if(num == 10):
+#         num_extenso = 'dez'
+#     elif(num == 9):
+#         num_extenso = 'nove'
+#     elif(num == 8):
+#         num_extenso = 'oito'
+#     elif(num == 7):
+#         num_extenso = 'sete'
+#     elif(num == 6):
+#         num_extenso = 'seis'
+#     elif(num == 5):
+#         num_extenso = 'cinco'
+#     elif(num == 4):
+#         num_extenso = 'quatro'
+#     elif(num == 3):
+#         num_extenso = 'três'
+#     elif(num == 2):
+#         num_extenso = 'dois'
+#     elif(num == 1):
+#         num_extenso = 'um'
+#     else:
+#         num_extenso = 'zero'
+# print(num_extenso)
+
+"""Implementação usando um dicionário."""
+
+dict_numeros = {
+    'zero': 0,
+    'um': 1,
+    'dois': 2,
+    'três': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10
+}
+
+num_extenso = None
+
+while(num_extenso == None):
+    num = int(input('Digite um número: '))
+    if(num >= 0 and num <= 10):
+        for chave, valor in dict_numeros.items():
+            if(num == valor):
+                num_extenso = chave
     else:
-        num_extenso = 'zero'
+        print('Número inválido!')
 
 print(num_extenso)
