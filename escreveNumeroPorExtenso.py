57b9d6ec560d221de8b9ac65ecfab2ebaf341fbc
otaviozerotwo <deadplatypus@protonmail.com>
2022-02-15 20:10:08 -0300

    Implementada flag para sair do loop infinito

--- escreveNumeroPorExtenso.py
+++ escreveNumeroPorExtenso.py
@@ -12,13 +12,20 @@ Documentação da biblioteca: https://pypi.org/project/num2words/
 
 from num2words import num2words
 
-while(True):
-    try:
-        num = int(input('Digite um número: '))
-        num_extenso = num2words(num, lang='pt-BR')
+num = None
 
-        print(num_extenso)
-        print('-------------------')
-        
+while(num != 'q'):
+    try:
+        num = input('Digite um número (ou "q" para sair): ')
+
+        if(num == 'q'):
+            break
+        else:
+            num = int(num)
+            num_extenso = num2words(num, lang='pt-BR')
+
+            print(num_extenso)
+            print('------------------------------------')
+
     except ValueError:
-        print('Digite apenas números!')
+        print('Digite apenas números! (ou "q" para sair)')
